295200cf561ab707ce84ef105e00dfe678f6dee2
Thragg969 <mass.navarrete@duocuc.cl>
2026-04-08 17:47:59 -0400

    Primer Commit

---
+++ src/main/java/com/grupocordillera/kpi/CordilleraKpiServiceApplication.java
@@ -0,0 +1,13 @@
+package com.grupocordillera.kpi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CordilleraKpiServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CordilleraKpiServiceApplication.class, args);
+	}
+
+}
